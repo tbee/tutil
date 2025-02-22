9bee24bbe3e97adaabdd8ed1ba91e396586850c9
Tom Eugelink <tbee@tbee.org>
2025-02-22 10:14:53 +0100

    RateLimiter

---
+++ javaHome.cmd
@@ -0,0 +1,10 @@
+set JAVA_HOME=C:\Users\tom\.jdks\azul-23.0.1
+set PATH=%JAVA_HOME%\bin;%PATH%
+set PATH="C:\Program Files (x86)\WiX Toolset v3.14\bin";%PATH%
+
+
+rem set GRAALVM_HOME=C:\Users\tom\.jdks\graalvm-ce-21.0.2
+rem set PATH=%GRAAL_VM%\bin;%PATH%
+rem set JAVA_HOME=%GRAALVM_HOME%
+rem set PATH=%JAVA_HOME%\bin;%PATH%
+
---
+++ src/main/java/org/tbee/tutil/RateLimiter.java
@@ -0,0 +1,90 @@
+package org.tbee.tutil;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.System.Logger.Level.DEBUG;
+import static java.lang.System.Logger.Level.TRACE;
+
+/// Release at most N tokens in a given time frame T.
+/// This implementation does not spread the load, like the leaky bucket implementation does, but only limits the number of tokens within the time frame.
+/// So all tokens can be claimed at the start, but then the N+1 token will be released when T has passed since the first token was claimed.
+/// And N+2 token after T has passed since the 2nd token was claimed.
+/// This can result in bursts, but usually the algorithm using this RateLimiter will cause some spreading.
+///
+/// Example:
+///     RateLimiter rateLimiter = new RateLimiter(10, Duration.ofMinutes(1));
+///     while (true) {
+///         rateLimiter.claim();
+///     }
+public class RateLimiter {
+    private static final System.Logger LOG = System.getLogger(RateLimiter.class.getName());
+
+    private final String name;
+    private final Duration timeframe;
+    private final DelayQueue<RateLimiterToken> delayQueue;
+
+    /// @param size
+    /// @param timeframe
+    public RateLimiter(String name, int size, Duration timeframe) {
+        this.name = name;
+        this.timeframe = timeframe;
+        this.delayQueue = new DelayQueue<>();
+
+        // Populate the initial size.
+        LocalDateTime now = LocalDateTime.now();
+        for (int i = 0; i < size; i++) {
+            delayQueue.add(new RateLimiterToken(now));
+            LOG.log(TRACE, "{0}: Populating with token available at {1}", name, now);
+        }
+        LOG.log(DEBUG, "{0}: Populated with {1} tokens", name, delayQueue.size());
+    }
+
+    public void claim() {
+        claim(null);
+    }
+
+    public void claim(String reason) {
+        String reasonLog = reason == null ? "" : " for '" + reason + "'";
+        try {
+            // Obtain token for this session
+            LOG.log(TRACE, "{0}: Claiming token{1}, first available is {2}", name, reasonLog, delayQueue.peek().earliestReleaseTime);
+            RateLimiterToken token = delayQueue.take();
+            LOG.log(DEBUG, "{0}: Claimed token{1} available at {2}", name, reasonLog, token.earliestReleaseTime);
+
+            // Create new token for future
+            LocalDateTime earliestReleaseTime = LocalDateTime.now().plus(timeframe);
+            delayQueue.add(new RateLimiterToken(earliestReleaseTime));
+            LOG.log(TRACE, "{0}: Refilled with token available at {1}", name, earliestReleaseTime);
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static class RateLimiterToken implements Delayed {
+        private final LocalDateTime earliestReleaseTime;
+
+        RateLimiterToken(LocalDateTime earliestReleaseTime) {
+            this.earliestReleaseTime = earliestReleaseTime;
+        }
+
+        @Override
+        public int compareTo(Delayed o) {
+            return earliestReleaseTime.compareTo(((RateLimiterToken)o).earliestReleaseTime);
+        }
+
+        @Override
+        public long getDelay(TimeUnit unit) {
+            LocalDateTime now = LocalDateTime.now();
+            if (earliestReleaseTime.isBefore(now)) {
+                return 0;
+            }
+            Duration duration = Duration.between(now, earliestReleaseTime);
+            return unit.convert(duration);
+        }
+    }
+}

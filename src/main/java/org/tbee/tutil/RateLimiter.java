aa3ec749ff52d55348eba4f8bf1c8ba04fe873fa
Tom Eugelink <tbee@tbee.org>
2025-02-23 10:18:20 +0100

    Improved the API

--- src/main/java/org/tbee/tutil/RateLimiter.java
+++ src/main/java/org/tbee/tutil/RateLimiter.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.DelayQueue;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
@@ -15,13 +16,13 @@ import static java.lang.System.Logger.Level.TRACE;
 /// This implementation does not spread the load, like the leaky bucket implementation does, but only limits the number of tokens within the time frame.
 /// So all tokens can be claimed at the start, but then the N+1 token will be released when T has passed since the first token was claimed.
 /// And N+2 token after T has passed since the 2nd token was claimed.
-/// This can result in bursts, but usually the algorithm using this RateLimiter will cause some spreading.
+/// This can result in bursts, but that is exactly what is allowed. Usually the algorithm using this RateLimiter will cause some spreading.
 ///
 /// Example:
+/// ```java
 ///     RateLimiter rateLimiter = new RateLimiter(10, Duration.ofMinutes(1));
-///     while (true) {
-///         rateLimiter.claim();
-///     }
+///     rateLimiter.claim();
+/// ```
 public class RateLimiter {
     private static final System.Logger LOG = System.getLogger(RateLimiter.class.getName());
 
@@ -29,8 +30,12 @@ public class RateLimiter {
     private final Duration timeframe;
     private final DelayQueue<RateLimiterToken> delayQueue;
 
-    /// @param size
-    /// @param timeframe
+    /// Initialize the rate delimiter, assuming all tokens are immediately available.
+    /// It is possible to create an empty RateLimiter, and then manually add tokens with specific release moments.
+    ///
+    /// @param name the name to identify the RateLimiter in logging
+    /// @param size the initial size of the RateLimiter
+    /// @param timeframe the time frame over which the tokens are available.
     public RateLimiter(String name, int size, Duration timeframe) {
         this.name = name;
         this.timeframe = timeframe;
@@ -45,11 +50,26 @@ public class RateLimiter {
         LOG.log(DEBUG, "{0}: Populated with {1} tokens", name, delayQueue.size());
     }
 
-    public void claim() {
-        claim(null);
+    /// Returns the name of this RateLimiter
+    public String name() {
+        return name;
     }
 
-    public void claim(String reason) {
+    /// Returns the number of tokens in this RateLimiter.
+    public int size() {
+        return delayQueue.size();
+    }
+
+    /// Returns first LocalDateTime that a token will be available.
+    public LocalDateTime peek() {
+        return Objects.requireNonNull(delayQueue.peek()).earliestReleaseTime;
+    }
+
+    public LocalDateTime claim() {
+        return claim(null);
+    }
+
+    public LocalDateTime claim(String reason) {
         String reasonLog = reason == null ? "" : " for '" + reason + "'";
         try {
             // Obtain token for this session
@@ -61,36 +81,43 @@ public class RateLimiter {
             LocalDateTime earliestReleaseTime = LocalDateTime.now().plus(timeframe);
             delayQueue.add(new RateLimiterToken(earliestReleaseTime));
             LOG.log(TRACE, "{0}: Refilled with token available at {1}", name, earliestReleaseTime);
+
+            // return what token was claimed
+            return token.earliestReleaseTime;
         }
         catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public List<RateLimiterToken> reduceTo(int size) {
-        List<RateLimiterToken> removed = new ArrayList<>();
+    /// Remove tokens from this RateLimiter until the size matches the parameter, latest tokens first.
+    /// The removed tokens can be added to another RateLimiter responsible for the same rate limit (e.g. in a segmented setup).
+    public List<LocalDateTime> reduceTo(int size) {
+        List<LocalDateTime> removed = new ArrayList<>();
         while (delayQueue.size() > size) {
             RateLimiterToken[] tokens = delayQueue.toArray(new RateLimiterToken[0]);
             if (tokens.length > 1) {
                 RateLimiterToken token = tokens[tokens.length - 1];
                 LOG.log(DEBUG, "{0}: Reducing to {1}, currently at {2}, so removing token available at {3}", name, size, tokens.length, token.earliestReleaseTime);
                 if (delayQueue.remove(token)) {
-                    removed.add(token);
+                    removed.add(token.earliestReleaseTime);
                 }
             }
         }
         return removed;
     }
 
-    public void add(List<RateLimiterToken> tokens) {
-        tokens.forEach(t -> delayQueue.add(t));
+    /// Add tokens to this RateLimiter.
+    /// Usually these were first removed from another RateLimiter responsible for the same rate limit (e.g. in a segmented setup).
+    public void add(List<LocalDateTime> tokens) {
+        tokens.forEach(ldt -> delayQueue.add(new RateLimiterToken(ldt)));
         LOG.log(DEBUG, "{0}: Added {1} tokens, new size = {2}", name, tokens.size(), delayQueue.size());
     }
 
-    public static class RateLimiterToken implements Delayed {
+    private static class RateLimiterToken implements Delayed {
         private final LocalDateTime earliestReleaseTime;
 
-        RateLimiterToken(LocalDateTime earliestReleaseTime) {
+        private RateLimiterToken(LocalDateTime earliestReleaseTime) {
             this.earliestReleaseTime = earliestReleaseTime;
         }
 
